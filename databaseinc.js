f20c5e4811fa0a05322dc5158ab2488e09b1a573
David Kučera <kucera.david2002@gmail.com>
2023-11-15 18:15:47 +0100

    ordering drivers by points, refactoring

---
+++ databaseinc.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://cbpdfyauboisozcxuohi.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNicGRmeWF1Ym9pc296Y3h1b2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5NTIwODEsImV4cCI6MjAxNTUyODA4MX0.A1DXi3mrghJdLH2j87DLsbyG8aPdPQLPbO9IGpxqTO0'
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
