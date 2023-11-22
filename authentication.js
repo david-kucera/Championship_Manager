a2840c2032a76a616e11e49f8b585617f8838825
David Kučera <kucera.david2002@gmail.com>
2023-11-22 09:51:21 +0100

    implemented different UI when user is logged in

--- authentication.js
+++ authentication.js
@@ -1,14 +1,21 @@
 let isAuthenticated = document.cookie.includes("isAuthenticated=true");
+const loginLogoutFooter = document.getElementById('login-logout-footer');
+
 if (isAuthenticated) {
     document.getElementById('login-option').style.display = 'none';
     document.getElementById('profile-option').style.display = 'block';
     document.getElementById('championships-option').style.display = 'block';
     document.getElementById('last-race-option').style.display = 'block';
     document.getElementById('logout-option').style.display = 'block';
-} else {
+    loginLogoutFooter.textContent = 'Logout';
+    loginLogoutFooter.href = 'index.html';
+}
+else {
     document.getElementById('login-option').style.display = 'block';
     document.getElementById('profile-option').style.display = 'none';
     document.getElementById('championships-option').style.display = 'none';
     document.getElementById('last-race-option').style.display = 'none';
     document.getElementById('logout-option').style.display = 'none';
+    loginLogoutFooter.textContent = 'Login';
+    loginLogoutFooter.href = 'login.html';
 }
