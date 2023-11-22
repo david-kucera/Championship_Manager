3eb94de2cfc9c6cec1a84b526b2cb2bbe34110fe
David Kučera <kucera.david2002@gmail.com>
2023-11-22 09:34:45 +0100

    simple logout works

--- logout.js
+++ logout.js
@@ -1,19 +1,20 @@
 async function logout() {
     try {
-        // const { error } = await _supabase.auth.signOut();
+
+        const { error } = await _supabase.auth.signOut();
 
         document.cookie = "isAuthenticated=false; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-    //     if (!error) {
-    //         // Remove auth cookie
-    //         document.cookie = "isAuthenticated=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    //
-    //         // Redirect to the index
-    //         window.location.href = 'index.html';
-    //     } else {
-    //         console.error('Logout error:', error.message);
-    //     }
-    // } catch (error) {
-    //     console.error('Unexpected error during logout:', error.message);
+        if (!error) {
+            // Remove auth cookie
+            document.cookie = "isAuthenticated=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+            // Redirect to the index
+            window.location.href = 'index.html';
+        } else {
+            console.error('Logout error:', error.message);
+        }
+    } catch (error) {
+         console.error('Unexpected error during logout:', error.message);
     }
 }
