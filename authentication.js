e1b1f75e63e3403f1fd2def0e5f7400f4e9ac564
David Kučera <kucera.david2002@gmail.com>
2023-11-23 15:21:25 +0100

    working logout for user

--- authentication.js
+++ authentication.js
@@ -1,21 +1,37 @@
 let isAuthenticated = document.cookie.includes("isAuthenticated=true");
 const loginLogoutFooter = document.getElementById('login-logout-footer');
 
-if (isAuthenticated) {
-    document.getElementById('login-option').style.display = 'none';
-    document.getElementById('profile-option').style.display = 'block';
-    document.getElementById('championships-option').style.display = 'block';
-    document.getElementById('last-race-option').style.display = 'block';
-    document.getElementById('logout-option').style.display = 'block';
-    loginLogoutFooter.textContent = 'Logout';
-    loginLogoutFooter.href = 'index.html';
+function setLoginLogoutLink() {
+    isAuthenticated = document.cookie.includes("isAuthenticated=true");
+
+    if (isAuthenticated) {
+        document.getElementById('login-option').style.display = 'none';
+        document.getElementById('profile-option').style.display = 'block';
+        document.getElementById('championships-option').style.display = 'block';
+        document.getElementById('last-race-option').style.display = 'block';
+        document.getElementById('logout-option').style.display = 'block';
+        loginLogoutFooter.textContent = 'Logout';
+        loginLogoutFooter.href = 'index.html';
+    } else {
+        document.getElementById('login-option').style.display = 'block';
+        document.getElementById('profile-option').style.display = 'none';
+        document.getElementById('championships-option').style.display = 'none';
+        document.getElementById('last-race-option').style.display = 'none';
+        document.getElementById('logout-option').style.display = 'none';
+        loginLogoutFooter.textContent = 'Login';
+        loginLogoutFooter.href = 'login.html';
+    }
 }
-else {
-    document.getElementById('login-option').style.display = 'block';
-    document.getElementById('profile-option').style.display = 'none';
-    document.getElementById('championships-option').style.display = 'none';
-    document.getElementById('last-race-option').style.display = 'none';
-    document.getElementById('logout-option').style.display = 'none';
-    loginLogoutFooter.textContent = 'Login';
-    loginLogoutFooter.href = 'login.html';
+
+function toggleLoginLogout() {
+    if (isAuthenticated) {
+        // User is logged in, perform logout
+        logout();
+    } else {
+        // User is not logged in, redirect to the login page
+        window.location.href = "login.html";
+    }
 }
+
+// Initial setup
+setLoginLogoutLink();
