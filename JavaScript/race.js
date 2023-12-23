29eefc9703946c11e8f6eb136bd6cc90b514b41d
kucera <kucera.david2002@gmail.com>
2023-12-23 10:13:13 +0100

    shows race data on race page

--- JavaScript/race.js
+++ JavaScript/race.js
@@ -1,57 +1,44 @@
 // Function to parse the URL and load the championship data
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
-    const championshipId = urlParams.get('championshipId');
+    const raceId = urlParams.get('raceId');
 
-    if (championshipId) {
-        fetchAndDisplayChampionshipData(championshipId);
+    if (raceId) {
+        fetchAndDisplayRaceData(raceId);
     } else {
-        console.error('No championship ID provided in the URL');
+        console.error('No race ID provided in the URL');
     }
 });
 
-async function fetchAndDisplayChampionshipData(championshipId) {
-    const { data, error } = await _supabase.from('championships').select('*').eq('id', championshipId);
+async function fetchAndDisplayRaceData(raceId) {
+    const { data, error } = await _supabase.from('races').select('*').eq('id', raceId);
 
     if (error) {
-        console.error('Error fetching championship data:', error.message);
+        console.error('Error fetching race data:', error.message);
         return;
     }
 
-    const championship = data[0];
-    if (championship) {
-        document.title = championship.name + " | Championship Manager";
-        document.getElementById('championship-name').textContent = championship.name;
-        document.getElementById('start-date').textContent = championship.startDate;
-        document.getElementById('end-date').textContent = championship.endDate;
-        document.getElementById('description').textContent = championship.description;
+    const race = data[0];
+    if (race) {
+        document.title = race.name + " | Championship Manager";
+        document.getElementById('race-name').textContent = race.name;
+        document.getElementById('date').textContent = race.date;
+        document.getElementById('location').textContent = race.location;
+        document.getElementById('description').textContent = race.description;
     }
 
-    const { data: raceData, error: raceError } = await _supabase
-        .from('races')
-        .select('*')
-        .eq('championshipId', championshipId);
+    return;
 
-    if (raceError) {
-        console.error('Error fetching race data:', raceError.message);
-        openModal('Error fetching race data!');
-    }
-
-    for (const race of raceData) {
-        addRaceToTable(race);
-    }
 
     const { data: driversData, error: driversError } = await _supabase
-        .from('driversInChampionship')
+        .from('driversInRace')
         .select('driverUid')
-        .eq('championshipId', championshipId);
+        .eq('raceId', raceId);
 
     if (driversError) {
         console.error('Error fetching drivers data:', driversError.message);
         openModal("Error fetching drivers data!");
     }
-
-
 
     let currentUserIsRegistered = false;
     const currentUserUid = getCookie('uid');
@@ -72,7 +59,7 @@ async function fetchAndDisplayChampionshipData(championshipId) {
             continue; // Skip this driver and continue with the next
         }
 
-        addDriverToTable(profileData[0].fullname, uid);
+        addDriverToTable(profileData[0].fullname);
     }
 
     // Hide the button if he is in the championship
@@ -89,31 +76,23 @@ async function fetchAndDisplayChampionshipData(championshipId) {
 function addRaceToTable(race) {
     const tbody = document.getElementById('tbody_championship_races');
     const row = document.createElement('tr');
-
     const nameCell = document.createElement('td');
-    const link = document.createElement('a');
-    link.href = `race.html?raceId=${race.id}`;
-    link.textContent = race.name;
-    nameCell.appendChild(link);
+    nameCell.textContent = race.name;
     const dateCell = document.createElement('td');
     dateCell.textContent = race.date;
     const locationCell = document.createElement('td');
     locationCell.textContent = race.location;
-
     row.appendChild(nameCell);
     row.appendChild(dateCell);
     row.appendChild(locationCell);
     tbody.appendChild(row);
 }
 
-function addDriverToTable(driverName, uid) {
+function addDriverToTable(driverName) {
     const tbody = document.getElementById('tbody_championship_drivers');
     const row = document.createElement('tr');
     const nameCell = document.createElement('td');
-    const link = document.createElement('a');
-    link.href = `driver_profile.html?driverUid=${uid}`;
-    link.textContent = driverName;
-    nameCell.appendChild(link);
+    nameCell.textContent = driverName;
     row.appendChild(nameCell);
     tbody.appendChild(row);
 }
