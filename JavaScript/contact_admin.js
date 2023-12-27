f377522db9e8c28a219f1915067a68a26f7eb6cf
kucera <kucera.david2002@gmail.com>
2023-12-27 11:29:06 +0100

    implemented admin page of contact form submissions

---
+++ JavaScript/contact_admin.js
@@ -0,0 +1,53 @@
+async function getMessages() {
+    const { data, error } = await _supabase
+        .from('contactFormSubmissions')
+        .select('*');
+    if (error) {
+        console.log(error.message);
+        openModal("An error occured while fetching data! Try again.");
+        return;
+    }
+
+    const messageList = document.getElementById('messageList');
+    messageList.innerHTML = '';
+
+    data.forEach(message => {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.innerHTML = `
+            <p><strong>Email:</strong> ${message.userEmail}</p>
+            <p><strong>Time:</strong> ${new Date(message.timestamp).toLocaleString()}</p>
+            <p><strong>Message:</strong> ${message.message}</p>
+            <a href="mailto:${message.userEmail}" class="btn btn-primary reply-btn">Reply</a>
+            <button class="btn btn-danger delete-btn" data-id="${message.id}">Delete</button>
+            <hr>
+        `;
+        messageList.appendChild(messageElement);
+    });
+
+    document.querySelectorAll('.delete-btn').forEach(button => {
+        button.addEventListener('click', function() {
+            deleteMessage(this.getAttribute('data-id'));
+        });
+    });
+}
+
+async function deleteMessage(messageId) {
+    const { error } = await _supabase
+        .from('contactFormSubmissions')
+        .delete()
+        .match({ id: messageId });
+    if (error) {
+        console.log(error.message);
+        openModal("Error deleting message!");
+    } else {
+        getMessages();
+    }
+}
+
+if (isAdmin) {
+    getMessages();
+} else {
+    $('#messages').hide();
+    openModal("You are not an admin! Go away!");
+}
